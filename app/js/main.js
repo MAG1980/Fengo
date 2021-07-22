2439a8f5b9bfd9c6fe06d5eb49d76cd8ebacc5fc
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2021-07-22 16:01:23 +0300

    Добавил скрипт прокрутки вверх.

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
-$(function(){
-    // $('body').css('color', '#fff')
-})
+var timeOut;
+function goUp() {
+  var top = Math.max(
+    document.body.scrollTop,
+    document.documentElement.scrollTop
+  );
+  if (top > 0) {
+    window.scrollBy(0, -100);
+    timeOut = setTimeout("goUp()", 20);
+  } else clearTimeout(timeOut);
+}
